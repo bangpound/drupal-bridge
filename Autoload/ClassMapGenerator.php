f7061cc367d0de3b6d7cb4d3ec3853de24a64b36
Benjamin Doherty <bjd@bangpound.org>
2014-03-29 09:43:29 -0500

    more movement.

---
+++ Autoload/ClassMapGenerator.php
@@ -0,0 +1,130 @@
+<?php
+namespace Bangpound\Bridge\Drupal\Autoload;
+
+use Composer\Autoload\ClassMapGenerator as BaseGenerator;
+use Symfony\Component\Finder\Finder;
+
+class ClassMapGenerator extends BaseGenerator
+{
+    /**
+     * File extensions to scan for PHP classes, intefaces and traits.
+     *
+     * @var array
+     */
+    private static $extensions = array('php', 'inc', 'module', 'theme', 'profile');
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function dump($dirs, $file)
+    {
+        $maps = array();
+
+        foreach ($dirs as $dir) {
+            $maps = array_merge($maps, static::createMap($dir));
+        }
+
+        if (!empty($maps)) {
+            file_put_contents($file, sprintf('<?php return %s;', var_export($maps, true)));
+        } elseif (file_exists($file)) {
+            unlink($file);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function createMap($path, $whitelist = null)
+    {
+        if (is_string($path)) {
+            if (is_file($path)) {
+                $path = array(new \SplFileInfo($path));
+            } elseif (is_dir($path)) {
+                $path = Finder::create()->files()->followLinks()->name('/\.('. implode('|', self::$extensions) .')$/')->in($path);
+            } else {
+                throw new \RuntimeException(
+                    'Could not scan for classes inside "'.$path.
+                    '" which does not appear to be a file nor a folder'
+                );
+            }
+        }
+
+        $map = array();
+
+        /** @var \Symfony\Component\Finder\SplFileInfo $file */
+        foreach ($path as $file) {
+            $filePath = $file->getRealPath();
+
+            if (!in_array(pathinfo($filePath, PATHINFO_EXTENSION), self::$extensions)) {
+                continue;
+            }
+
+            if ($whitelist && !preg_match($whitelist, strtr($filePath, '\\', '/'))) {
+                continue;
+            }
+
+            $classes = self::findClasses($filePath);
+
+            foreach ($classes as $class) {
+                $map[$class] = $filePath;
+            }
+        }
+
+        return $map;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    private static function findClasses($path)
+    {
+        $traits = version_compare(PHP_VERSION, '5.4', '<') ? '' : '|trait';
+
+        try {
+            $contents = php_strip_whitespace($path);
+        } catch (\Exception $e) {
+            throw new \RuntimeException('Could not scan for classes inside '.$path.": \n".$e->getMessage(), 0, $e);
+        }
+
+        // return early if there is no chance of matching anything in this file
+        if (!preg_match('{\b(?:class|interface'.$traits.')\s}i', $contents)) {
+            return array();
+        }
+
+        // strip heredocs/nowdocs
+        $contents = preg_replace('{<<<\'?(\w+)\'?(?:\r\n|\n|\r)(?:.*?)(?:\r\n|\n|\r)\\1(?=\r\n|\n|\r|;)}s', 'null', $contents);
+        // strip strings
+        $contents = preg_replace('{"[^"\\\\]*(\\\\.[^"\\\\]*)*"|\'[^\'\\\\]*(\\\\.[^\'\\\\]*)*\'}s', 'null', $contents);
+        // strip leading non-php code if needed
+        if (substr($contents, 0, 2) !== '<?') {
+            $contents = preg_replace('{^.+?<\?}s', '<?', $contents);
+        }
+        // strip non-php blocks in the file
+        $contents = preg_replace('{\?>.+<\?}s', '?><?', $contents);
+        // strip trailing non-php code if needed
+        $pos = strrpos($contents, '?>');
+        if (false !== $pos && false === strpos(substr($contents, $pos), '<?')) {
+            $contents = substr($contents, 0, $pos);
+        }
+
+        preg_match_all('{
+            (?:
+                 \b(?<![\$:>])(?P<type>class|interface'.$traits.') \s+ (?P<name>[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)
+               | \b(?<![\$:>])(?P<ns>namespace) (?P<nsname>\s+[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*(?:\s*\\\\\s*[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*)*)? \s*[\{;]
+            )
+        }ix', $contents, $matches);
+
+        $classes = array();
+        $namespace = '';
+
+        for ($i = 0, $len = count($matches['type']); $i < $len; $i++) {
+            if (!empty($matches['ns'][$i])) {
+                $namespace = str_replace(array(' ', "\t", "\r", "\n"), '', $matches['nsname'][$i]) . '\\';
+            } else {
+                $classes[] = ltrim($namespace . $matches['name'][$i], '\\');
+            }
+        }
+
+        return $classes;
+    }
+}
--- Composer/ScriptHandler.php
+++ Composer/ScriptHandler.php
@@ -2,7 +2,9 @@
 
 namespace Bangpound\Bridge\Drupal\Composer;
 
+use Bangpound\Bridge\Drupal\Autoload\ClassMapGenerator;
 use Composer\Script\CommandEvent;
+use Symfony\Component\EventDispatcher\Event;
 use Symfony\Component\Filesystem\Filesystem;
 
 class ScriptHandler
@@ -55,6 +57,7 @@ class ScriptHandler
             array(
                 'symfony-web-dir' => 'web',
                 'symfony-drupal-install' => 'relative',
+                'drupal-root' => '',
             ),
             $event->getComposer()->getPackage()->getExtra()
         );
@@ -62,5 +65,68 @@ class ScriptHandler
         $options['symfony-drupal-install'] = getenv('SYMFONY_DRUPAL_INSTALL') ?: $options['symfony-drupal-install'];
 
         return $options;
+    }
+
+
+    /**
+     * Paths in Drupal root to scan for classes.
+     *
+     * This should never include profiles and sites, because those are scanned to
+     * generate separate classmaps.
+     *
+     * @var array
+     */
+    protected static $root_paths = array(
+        'includes', 'misc', 'modules', 'scripts', 'themes',
+
+        // None of these files actually contain PHP classes, but the are
+        // scanned anyway.
+        'authorize.php', 'cron.php', 'index.php', 'install.php',
+        'update.php', 'xmlrpc.php',
+    );
+
+    /**
+     * Paths in subdirectories (profiles and sites) to scan for classes.
+     *
+     * @var array
+     */
+    protected static $subdir_paths = array(
+        'modules', 'themes', 'plugins',
+    );
+
+    public static function dumpAutoload(Event $event)
+    {
+        $cwd = getcwd();
+        $io = $event->getIO();
+
+        $options = self::getOptions($event);
+        if (!empty($options['drupal-root'])) {
+            chdir($options['drupal-root']);
+        }
+
+        $generator = new ClassMapGenerator();
+        $dirs = array_filter(self::$root_paths, 'file_exists');
+        $io->write('Dumping classmap for <info>DRUPAL_ROOT</info>');
+        $generator->dump($dirs, 'classmap.php');
+
+        $finder = Finder::create()
+            ->directories()
+            ->depth(0)
+            ->followLinks()
+            ->in(array('profiles', 'sites'))
+        ;
+
+        foreach ($finder as $file) {
+            /** @var \Symfony\Component\Finder\SplFileInfo $file */
+            chdir($file->getPathInfo() .'/'. $file->getRelativePathname());
+            $io->write(sprintf('Dumping classmap for <info>%s</info>', $file->getPathInfo() .'/'. $file->getRelativePathname()));
+            $dirs = array_filter(self::$subdir_paths, 'file_exists');
+            $generator->dump($dirs, 'classmap.php');
+            chdir($cwd);
+            if (!empty($options['drupal-root'])) {
+                chdir($options['drupal-root']);
+            }
+        }
+        chdir($cwd);
     }
 }
