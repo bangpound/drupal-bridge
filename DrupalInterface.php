63dbfdd2d10173873b3e3f4dec7c416914aad262
Benjamin Doherty <bjd@bangpound.org>
2014-04-02 13:49:57 -0500

    constants cannot be overridden.

--- DrupalInterface.php
+++ DrupalInterface.php
@@ -5,21 +5,6 @@ namespace Bangpound\Bridge\Drupal;
 interface DrupalInterface
 {
     /**
-     * The current system version.
-     */
-    const VERSION = '0.0-dev';
-
-    /**
-     * Core API compatibility.
-     */
-    const CORE_COMPATIBILITY = '7.x';
-
-    /**
-     * Core minimum schema version.
-     */
-    const CORE_MINIMUM_SCHEMA_VERSION = 7000;
-
-    /**
      * Returns true if the service id is defined.
      *
      * @param string $id The service id
